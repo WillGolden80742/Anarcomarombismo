7e579d6cf711e2ed373e32bc117505aa134d31d7
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2024-09-05 23:39:40 -0300

    up

--- app/src/main/java/com/example/anarcomarombismo/formDailyCalories.kt
+++ app/src/main/java/com/example/anarcomarombismo/formDailyCalories.kt
@@ -153,7 +153,6 @@ class formDailyCalories : AppCompatActivity() {
                 seeFoodsButton.isEnabled = false
             }
             currentFood = null
-            // saveDailyCalories()
         }
     }
 
