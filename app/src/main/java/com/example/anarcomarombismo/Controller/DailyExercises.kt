3e02bb478b9ad63c19c3495da0b57b871a263cfd
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2024-09-16 15:03:56 -0300

    up

--- app/src/main/java/com/example/anarcomarombismo/Controller/DailyExercises.kt
+++ app/src/main/java/com/example/anarcomarombismo/Controller/DailyExercises.kt
@@ -5,7 +5,6 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
-
 class DailyExercises (context: Context) {
     private val context = context
     private val jsonUtil = JSON()
