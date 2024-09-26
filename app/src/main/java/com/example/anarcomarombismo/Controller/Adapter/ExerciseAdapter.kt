8a48648eaa0fff784a83080903d3f21eac5b5dab
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2024-09-26 00:12:17 -0300

    up

--- app/src/main/java/com/example/anarcomarombismo/Controller/Adapter/ExerciseAdapter.kt
+++ app/src/main/java/com/example/anarcomarombismo/Controller/Adapter/ExerciseAdapter.kt
@@ -2,7 +2,6 @@ package com.example.anarcomarombismo.Controller.Adapter
 
 import android.content.Context
 import android.content.Intent
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
