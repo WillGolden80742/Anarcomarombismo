5875bf97e576a8d22f6185af64bf473ca3c86c77
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2024-09-29 11:01:53 -0300

    up

---
+++ app/src/main/java/com/example/anarcomarombismo/Forms/formMacros.kt
@@ -0,0 +1,16 @@
+package com.example.anarcomarombismo.Forms
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.anarcomarombismo.R
+
+class formMacros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_form_macros)
+    }
+}
--- app/src/main/java/com/example/anarcomarombismo/dailyCalories.kt
+++ app/src/main/java/com/example/anarcomarombismo/dailyCalories.kt
@@ -4,24 +4,57 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.TextView
+import androidx.core.view.isVisible
 import com.example.anarcomarombismo.Controller.Adapter.DailyCaloriesAdapter
 import com.example.anarcomarombismo.Controller.DailyCalories
 import com.example.anarcomarombismo.Forms.formDailyCalories
 import com.example.anarcomarombismo.Forms.formFood
+import com.example.anarcomarombismo.Forms.formMacros
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class dailyCalories : AppCompatActivity() {
     private lateinit var caloriesFoodList: ListView
     private lateinit var addCaloriesButton: Button
     private lateinit var addNewFoodButton: Button
+    private lateinit var progressContainer: LinearLayout
+    private lateinit var showStatisticsButton: FloatingActionButton
+    private lateinit var editStatisticsButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_calories)
         caloriesFoodList = findViewById(R.id.caloriesFoodList)
         addCaloriesButton = findViewById(R.id.addFoodFormButton)
         addNewFoodButton = findViewById(R.id.addNewFoodButton)
+        progressContainer = findViewById(R.id.progressContainer)
+        showStatisticsButton = findViewById(R.id.showStatisticsButton)
+        editStatisticsButton = findViewById(R.id.editStatisticsButton)
         addCaloriesButton.setOnClickListener {
             callFormDailyCalories()
+        }
+        showStatisticsButton.setOnClickListener {
+            if (!progressContainer.isVisible) {
+                progressContainer.isVisible = true
+                editStatisticsButton.isVisible = true
+                showStatisticsButton.setImageResource(R.drawable.ic_fluent_dismiss_24_filled)
+            } else {
+                progressContainer.isVisible = false
+                editStatisticsButton.isVisible = false
+                showStatisticsButton.setImageResource(R.drawable.ic_fluent_arrow_growth_24_filled)
+            }
+        }
+
+        progressContainer.setOnClickListener {
+            val intent = Intent(this, formMacros::class.java)
+            startActivity(intent)
+        }
+
+        editStatisticsButton.setOnClickListener {
+            val intent = Intent(this, formMacros::class.java)
+            startActivity(intent)
         }
 
         caloriesFoodList.setOnItemClickListener { parent, _, position, _ ->
@@ -40,6 +73,9 @@ class dailyCalories : AppCompatActivity() {
         }
     }
     override fun onResume() {
+        progressContainer.isVisible = false
+        editStatisticsButton.isVisible = false
+        showStatisticsButton.setImageResource(R.drawable.ic_fluent_arrow_growth_24_filled)
         super.onResume()
         caloriesFoodList.adapter = DailyCaloriesAdapter(this,
             DailyCalories().fetchAll(this).ifEmpty {
@@ -47,7 +83,6 @@ class dailyCalories : AppCompatActivity() {
             }
         )
     }
-
     private fun callFormDailyCalories() {
         try {
             startActivity(Intent(this, formDailyCalories::class.java))
