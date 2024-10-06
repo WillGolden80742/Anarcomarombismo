12767abcdbde53772a3606f013b90eaf965d97a8
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2024-10-06 00:39:58 -0300

    up

--- app/src/main/java/com/example/anarcomarombismo/Forms/formBMR.kt
+++ app/src/main/java/com/example/anarcomarombismo/Forms/formBMR.kt
@@ -39,72 +39,50 @@ class formBMR : AppCompatActivity() {
     }
 
     private fun fetchBMRProfile() {
-        if (BasalMetabolicRate().hasBasalMetabolicRate(this)) {
-            BasalMetabolicRate().fetch(this).let {
-                editTextWeight.setText(it!!.weight.toString())
-                editTextHeight.setText(it!!.height.toString())
-                editTextAge.setText(it!!.age.toString())
+        BasalMetabolicRate().takeIf { it.hasBasalMetabolicRate(this) }?.fetch(this)?.let {
+            editTextWeight.setText(it.weight.toString())
+            editTextHeight.setText(it.height.toString())
+            editTextAge.setText(it.age.toString())
+
+            spinnerGender.setSelection(
                 when (it.gender) {
-                    "M" -> {
-                        spinnerGender.setSelection(1)
-                    }
-                    "F" -> {
-                        spinnerGender.setSelection(2)
-                    }
-
+                    "M" -> 1
+                    "F" -> 2
+                    else -> 0
                 }
+            )
+
+            spinnerActivityLevel.setSelection(
                 when (it.activityLevel) {
-                    1.2 -> {
-                        spinnerActivityLevel.setSelection(1)
-                    }
-                    1.375 -> {
-                        spinnerActivityLevel.setSelection(2)
-                    }
-                    1.55 -> {
-                        spinnerActivityLevel.setSelection(3)
-                    }
-                    1.725 -> {
-                        spinnerActivityLevel.setSelection(4)
-                    }
-                    1.9 -> {
-                        spinnerActivityLevel.setSelection(5)
-                    }
+                    1.2 -> 1
+                    1.375 -> 2
+                    1.55 -> 3
+                    1.725 -> 4
+                    1.9 -> 5
+                    else -> 0
                 }
-
-            }
+            )
         }
     }
     private fun saveBMRProfile() {
-        val weightInput = editTextWeight.text.toString()
-        if (weightInput.isEmpty()) {
-            Toast.makeText(this, getString(R.string.mandatory_weight), Toast.LENGTH_SHORT).show()
-            return
+        val weight = editTextWeight.text.toString().toDoubleOrNull().also {
+            if (it == null) {
+                Toast.makeText(this, getString(R.string.mandatory_weight), Toast.LENGTH_SHORT).show()
+                return
+            }
         }
-        val weight = weightInput.toDoubleOrNull() ?: run {
-            Toast.makeText(this, getString(R.string.invalid_weight), Toast.LENGTH_SHORT).show()
-            return
+        val height = editTextHeight.text.toString().toIntOrNull().also {
+            if (it == null) {
+                Toast.makeText(this, getString(R.string.mandatory_height), Toast.LENGTH_SHORT).show()
+                return
+            }
         }
-
-        val heightInput = editTextHeight.text.toString()
-        if (heightInput.isEmpty()) {
-            Toast.makeText(this, getString(R.string.mandatory_height), Toast.LENGTH_SHORT).show()
-            return
+        val age = editTextAge.text.toString().toIntOrNull().also {
+            if (it == null) {
+                Toast.makeText(this, getString(R.string.mandatory_age), Toast.LENGTH_SHORT).show()
+                return
+            }
         }
-        val height = heightInput.toIntOrNull() ?: run {
-            Toast.makeText(this, getString(R.string.height_invalid), Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        val ageInput = editTextAge.text.toString()
-        if (ageInput.isEmpty()) {
-            Toast.makeText(this, getString(R.string.mandatory_age), Toast.LENGTH_SHORT).show()
-            return
-        }
-        val age = ageInput.toIntOrNull() ?: run {
-            Toast.makeText(this, getString(R.string.invalid_age), Toast.LENGTH_SHORT).show()
-            return
-        }
-
         val genderMultiplier = when (spinnerGender.selectedItemId) {
             1L -> "M"
             2L -> "F"
@@ -113,7 +91,6 @@ class formBMR : AppCompatActivity() {
                 return
             }
         }
-
         val activityLevel = when (spinnerActivityLevel.selectedItemId) {
             1L -> 1.2
             2L -> 1.375
@@ -126,13 +103,12 @@ class formBMR : AppCompatActivity() {
             }
         }
         val bmr = BasalMetabolicRate.build(
-            weight = weight,
-            height = height,
-            age = age,
+            weight = weight ?: 0.0,
+            height = height ?: 0,
+            age = age ?: 0,
             gender = genderMultiplier,
             activityLevel = activityLevel
         )
-
         if (bmr.save(this)) {
             finish()
         }
